6fac2362a214b0fcc3b3e24c5a951dc4296c48f9
Antoine Beaupré <anarcat@debian.org>
2024-11-19 16:38:45 -0500

    refactor config class into a define
    This leaves the actual class unchanged, but allows reusing the file
    creation logic to create more config files, by default in a conf.d
    directory.

--- manifests/config.pp
+++ manifests/config.pp
@@ -9,25 +9,9 @@ class pgbackrest::config(
   String $filename   = '/etc/pgbackrest.conf',
   Boolean $show_diff = true,
 ) {
-  # Add each section block configs
-  $pgbackrest::config.each |String $section, Hash $settings| {
-    $settings.each |String $name, String $value| {
-      # Remove values not defined or empty
-      $is_present = $value ? {
-        undef   => 'absent',
-        ''      => 'absent',
-        default => 'present',
-      }
-
-      # Write the configuration options to pgbackrest::config::filename
-      ini_setting { "${section} ${name}":
-        ensure    => $is_present,
-        path      => $filename,
-        section   => $section,
-        setting   => $name,
-        value     => $value,
-        show_diff => $show_diff,
-      }
-    }
+  pgbackrest::config_file { $filename:
+    filename  => $filename,
+    show_diff => $show_diff,
+    config    => $pgbackrest::config,
   }
 }
